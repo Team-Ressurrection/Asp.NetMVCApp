c8f227b4e5cbdae2324f9e401625d79947fe6bf2
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-04 13:32:02 +0200

    add Ninject configuration

---
+++ SalaryCalculator/SalaryCalculatorWeb/App_Start/SalaryCalculatorNinjectModule.cs
@@ -0,0 +1,33 @@
+﻿using Ninject.Modules;
+using Ninject.Web.Common;
+
+using SalaryCalculator.Data;
+using SalaryCalculator.Data.Contracts;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Repositories;
+using SalaryCalculator.Data.Services;
+using SalaryCalculator.Data.Services.Contracts;
+
+namespace SalaryCalculatorWeb.App_Start
+{
+    public class SalaryCalculatorNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<ISalaryCalculatorDbContext>().To<SalaryCalculatorDbContext>().InRequestScope();
+            this.Bind(typeof(IRepository<>)).To(typeof(SalaryCalculatorRepository<>)).InSingletonScope();
+
+            this.Bind<IRemunerationBillService>().To<RemunerationBillService>().InRequestScope();
+            this.Bind<IEmployeePaycheckService>().To<EmployeePaycheckService>().InRequestScope();
+            this.Bind<IUserService>().To<UserService>().InRequestScope();
+            this.Bind<ISelfEmploymentService>().To<SelfEmploymentService>().InRequestScope();
+            this.Bind<IEmployeeService>().To<EmployeeService>().InRequestScope();
+
+            this.Bind<User>().ToSelf();
+            this.Bind<Employee>().ToSelf();
+            this.Bind<RemunerationBill>().ToSelf();
+            this.Bind<EmployeePaycheck>().ToSelf();
+            this.Bind<SelfEmployment>().ToSelf();
+        }
+    }
+}
