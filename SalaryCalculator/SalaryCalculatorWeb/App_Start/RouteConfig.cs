e800f65c0af5a303be21536d0ca37ae2c351bdfa
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-07 00:56:41 +0200

    add lowerCaseUrls

--- SalaryCalculator/SalaryCalculatorWeb/App_Start/RouteConfig.cs
+++ SalaryCalculator/SalaryCalculatorWeb/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace SalaryCalculatorWeb
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.LowercaseUrls = true;
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
