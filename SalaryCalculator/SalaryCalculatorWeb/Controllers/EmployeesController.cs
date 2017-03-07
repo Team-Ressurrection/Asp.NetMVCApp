2e45a5036fae8dca50786b8c398d849fc865705f
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-08 01:09:14 +0200

    add EmployeesController and View to all employees

---
+++ SalaryCalculator/SalaryCalculatorWeb/Controllers/EmployeesController.cs
@@ -0,0 +1,100 @@
+﻿using Bytes2you.Validation;
+using SalaryCalculator.Data.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalaryCalculatorWeb.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeService employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            Guard.WhenArgument(employeeService, "employeeService").IsNull().Throw();
+
+            this.employeeService = employeeService;
+        }
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(this.employeeService.GetAll().ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employees/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SalaryCalculator/SalaryCalculatorWeb/Models/SettingsViewModels/EmployeeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalaryCalculatorWeb.Models.SettingsViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PersonalId { get; set; }
+    }
+}
