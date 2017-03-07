092bfdcb312dda17f5558bc12ffbe852edb028ee
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-08 01:09:31 +0200

    add simple employeesController tests

---
+++ SalaryCalculator/SalaryCalculatorWeb.Tests/Controllers/EmployeeControllerTests/Constructor_Should.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using Moq;
+using NUnit.Framework;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Services.Contracts;
+using SalaryCalculator.Tests.Mocks;
+using SalaryCalculatorWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorWeb.Tests.Controllers.EmployeeControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void CreateInstance_WhenAllParametersAreSetUpCorrectly()
+        {
+            // Arrange
+            var employeeService = new Mock<IEmployeeService>();
+
+            // Act
+            EmployeesController emplController = new EmployeesController(employeeService.Object);
+
+            // Assert
+            Assert.IsInstanceOf<EmployeesController>(emplController);
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenEmployeeServiceIsNull()
+        {
+            // Arrange
+            IEmployeeService nullableEmployeeService = null;
+
+            // Act & Assert
+            Assert.That(() => new EmployeesController(nullableEmployeeService), Throws.InstanceOf<ArgumentNullException>().With.Message.Contains("The argument is null"));
+        }
+    }
+}
---
+++ SalaryCalculator/SalaryCalculatorWeb.Tests/Controllers/EmployeeControllerTests/Index_Should.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using NUnit.Framework;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Services.Contracts;
+using SalaryCalculatorWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SalaryCalculatorWeb.Tests.Controllers.EmployeeControllerTests
+{
+    [TestFixture]
+    public class Index_Should
+    {
+        [Test]
+        public void ReturnViewResultAsListOfEmployees()
+        {
+            // Arrange
+            var employeeService = new Mock<IEmployeeService>();
+            EmployeesController emplController = new EmployeesController(employeeService.Object);
+
+            // Act
+            emplController.Index();
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(emplController.Index());
+        }
+    }
+}
