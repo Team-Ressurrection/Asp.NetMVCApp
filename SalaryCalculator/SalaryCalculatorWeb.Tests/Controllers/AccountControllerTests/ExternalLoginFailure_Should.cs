3d1161580fd903104da288e3f3b41014f27b8735
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-09 00:34:03 +0200

    add another test

---
+++ SalaryCalculator/SalaryCalculatorWeb.Tests/Controllers/AccountControllerTests/ExternalLoginFailure_Should.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Moq;
+using NUnit.Framework;
+using SalaryCalculator.Data.Models;
+using SalaryCalculatorWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SalaryCalculatorWeb.Tests.Controllers.AccountControllerTests
+{
+    [TestFixture]
+    public class ExternalLoginFailure_Should
+    {
+        [Test]
+        public void ReturnView_WhenIsCalled()
+        {
+            // Arrange
+            var mockedStore = new Mock<IUserStore<User>>();
+            var mockedUserManager = new Mock<ApplicationUserManager>(mockedStore.Object);
+
+            var mockedAuthenticationManager = new Mock<IAuthenticationManager>();
+            var mockedSignInManager = new Mock<ApplicationSignInManager>(mockedUserManager.Object, mockedAuthenticationManager.Object);
+            
+            // Act
+            var accController = new AccountController(mockedUserManager.Object, mockedSignInManager.Object);
+            ActionResult result = accController.ExternalLoginFailure() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
