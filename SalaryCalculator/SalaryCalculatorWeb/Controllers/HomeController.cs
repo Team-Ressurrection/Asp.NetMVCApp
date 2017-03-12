1566d1aef557461ff3ae148be70d1d09b7bdeaa9
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-12 03:12:10 +0200

    add PartialView in HomeController

--- SalaryCalculator/SalaryCalculatorWeb/Controllers/HomeController.cs
+++ SalaryCalculator/SalaryCalculatorWeb/Controllers/HomeController.cs
@@ -14,22 +14,43 @@ namespace SalaryCalculatorWeb.Controllers
     {
         private readonly ICacheService cacheService;
         private readonly IUserService userService;
+        private readonly IEmployeePaycheckService paycheckService;
+        private readonly IRemunerationBillService billService;
+        private readonly ISelfEmploymentService selfEmploymentService;
 
-        public HomeController(ICacheService cacheService, IUserService userService)
-        {
-            Guard.WhenArgument(cacheService, "cacheService").IsNull().Throw();
+        public HomeController(ICacheService cacheService, IUserService userService, IEmployeePaycheckService paycheckService, IRemunerationBillService billService, ISelfEmploymentService selfEmploymentService)
+        {                                                                           
+            Guard.WhenArgument(cacheService, "cacheService").IsNull().Throw();      
             Guard.WhenArgument(userService, "userService").IsNull().Throw();
+            Guard.WhenArgument(paycheckService, "paycheckService").IsNull().Throw();
+            Guard.WhenArgument(billService, "billService").IsNull().Throw();
+            Guard.WhenArgument(selfEmploymentService, "selfEmploymentService").IsNull().Throw();
 
             this.cacheService = cacheService;
             this.userService = userService;
+            this.paycheckService = paycheckService;
+            this.billService = billService;
+            this.selfEmploymentService = selfEmploymentService;
         }
 
-        public ActionResult Index(InfoViewModel infoViewModel)
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Information(InfoViewModel infoViewModel)
         {
             var registeredUsers = this.cacheService.Get("totalRegisteredUsers", () => this.userService.GetAll().Count(), 60);
+            var totalPaychecks = this.cacheService.Get("totalPaychecks", () => this.paycheckService.GetAll().Count(), 60);
+            var totalRemunerationBills = this.cacheService.Get("totalRemunerationBills", () => this.billService.GetAll().Count(), 60);
+            var totalSelfEmployments = this.cacheService.Get("totalSelfEmployments", () => this.paycheckService.GetAll().Count(), 60);
 
             infoViewModel.TotalRegisteredUsers = registeredUsers;
-            return View(infoViewModel);
+            infoViewModel.TotalPaychecks = totalPaychecks;
+            infoViewModel.TotalRemunerationBills = totalRemunerationBills;
+            infoViewModel.TotalSelfEmployments = totalSelfEmployments;
+
+            return this.PartialView("InformationPartial", infoViewModel);
         }
 
         public ActionResult About()
--- SalaryCalculator/SalaryCalculatorWeb/Models/HomeViewModels/InfoViewModel.cs
+++ SalaryCalculator/SalaryCalculatorWeb/Models/HomeViewModels/InfoViewModel.cs
@@ -8,5 +8,11 @@ namespace SalaryCalculatorWeb.Models.HomeViewModels
     public class InfoViewModel
     {
         public int TotalRegisteredUsers { get; set; }
+
+        public int TotalPaychecks { get; set; }
+
+        public int TotalRemunerationBills { get; set; }
+
+        public int TotalSelfEmployments { get; set; }
     }
 }
