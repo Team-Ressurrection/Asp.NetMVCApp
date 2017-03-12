af68c860f31bd0237130bd3917d111890e7fce51
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-12 03:24:21 +0200

    setup mocks

--- SalaryCalculator/SalaryCalculatorWeb.Tests/Controllers/HomeControllerTests/Information_Should.cs
+++ SalaryCalculator/SalaryCalculatorWeb.Tests/Controllers/HomeControllerTests/Information_Should.cs
@@ -27,7 +27,12 @@ namespace SalaryCalculatorWeb.Tests.Controllers.HomeControllerTests
             var selfEmploymentService = new Mock<ISelfEmploymentService>();
             HomeController controller = new HomeController(cacheService.Object, userService.Object, paycheckService.Object, billService.Object,selfEmploymentService.Object);
 
+            cacheService.Setup(x => x.Get("t1", () => userService.Object.GetAll().Count(), 60)).Returns(2);
+            cacheService.Setup(x => x.Get("t1", () => paycheckService.Object.GetAll().Count(), 60)).Returns(2);
+            cacheService.Setup(x => x.Get("t1", () => billService.Object.GetAll().Count(), 60)).Returns(2);
+            cacheService.Setup(x => x.Get("t1", () => selfEmploymentService.Object.GetAll().Count(), 60)).Returns(2);
             var infoViewModel = new Mock<InfoViewModel>();
+
             // Act
             PartialViewResult result = controller.Information(infoViewModel.Object) as PartialViewResult;
 
