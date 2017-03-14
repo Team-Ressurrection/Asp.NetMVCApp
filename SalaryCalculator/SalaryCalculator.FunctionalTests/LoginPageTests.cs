3479fb98b765cdd8de7869384807cffe4ca57a27
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-15 00:34:50 +0200

    add login and register page tests

---
+++ SalaryCalculator/SalaryCalculator.FunctionalTests/LoginPageTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator.FunctionalTests
+{
+    [TestFixture]
+   public class LoginPageTests
+    {
+        private IWebDriver driver;
+
+        [Test]
+        public void ShouldFillAllFieldsAndClickLoginButton()
+        {
+            this.driver = new ChromeDriver();
+            this.driver.Navigate().GoToUrl("http://www.salarycalculatormvc.com/account/login");
+
+            IWebElement userNameInput = this.driver.FindElement(By.Id("Email"));
+            userNameInput.SendKeys("Alexander");
+
+            IWebElement passwordInput = this.driver.FindElement(By.Id("Password"));
+            passwordInput.SendKeys("123456");
+
+            IWebElement loginButton = this.driver.FindElement(By.ClassName("btn-default"));
+            loginButton.Click();
+
+            this.driver.Quit();
+        }
+    }
+}
---
+++ SalaryCalculator/SalaryCalculator.FunctionalTests/RegistrationPageTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator.FunctionalTests
+{
+    [TestFixture]
+    public class RegistrationPageTests
+    {
+        private IWebDriver driver;
+
+        [Test]
+        public void ShouldEnterAllFieldsAndClickRegisterButton()
+        {
+            this.driver = new ChromeDriver();
+            this.driver.Navigate().GoToUrl("http://www.salarycalculatormvc.com/account/register");
+
+            IWebElement userNameInput = this.driver.FindElement(By.Id("Email"));
+            userNameInput.SendKeys("Alexander");
+
+            IWebElement companyNameInput = this.driver.FindElement(By.Id("CompanyName"));
+            companyNameInput.SendKeys("Payroll Ltd.");
+
+            IWebElement companyAddressInput = this.driver.FindElement(By.Id("CompanyAddress"));
+            companyAddressInput.SendKeys("bul. Bulgaria 90");
+
+            IWebElement passwordInput = this.driver.FindElement(By.Id("Password"));
+            passwordInput.SendKeys("123456");
+
+            IWebElement confirmPasswordInput = this.driver.FindElement(By.Id("ConfirmPassword"));
+            confirmPasswordInput.SendKeys("123456");
+
+            IWebElement registerButton = this.driver.FindElement(By.ClassName("btn-default"));
+            registerButton.Click();
+
+            this.driver.Quit();
+        }
+    }
+}
