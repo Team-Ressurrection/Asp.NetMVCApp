969934c753e6101e2402e233f446815f87acfd44
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-18 20:17:45 +0200

    add tests

---
+++ SalaryCalculator/SalaryCalculatorWeb.Tests/Controllers/NonLaborContractControllerTests/Constructor_Should.cs
@@ -0,0 +1,85 @@
+﻿using Moq;
+using NUnit.Framework;
+using SalaryCalculator.Configuration.Mappings;
+using SalaryCalculator.Data.Services.Contracts;
+using SalaryCalculator.Utilities.Calculations;
+using SalaryCalculatorWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorWeb.Tests.Controllers.NonLaborContractControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void CreateInstance_WhenAllServicesArePassedCorrectly()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            var billService = new Mock<IRemunerationBillService>();
+            var payrollCalculations = new Mock<Payroll>();
+            
+            // Act & Assert
+            Assert.IsInstanceOf<NonLaborContractController>(new NonLaborContractController(mapService.Object, employeeService.Object, billService.Object, payrollCalculations.Object));
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenMapServiceIsNull()
+        {
+            // Arrange
+            IMapService mapService = null;
+            var employeeService = new Mock<IEmployeeService>();
+            var billService = new Mock<IRemunerationBillService>();
+            var payrollCalculations = new Mock<Payroll>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new NonLaborContractController(mapService, employeeService.Object, billService.Object, payrollCalculations.Object));
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenEmployeeServiceIsNull()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            IEmployeeService employeeService = null;
+            var billService = new Mock<IRemunerationBillService>();
+            var payrollCalculations = new Mock<Payroll>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new NonLaborContractController(mapService.Object, employeeService, billService.Object, payrollCalculations.Object));
+
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenBillServiceIsNull()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            IRemunerationBillService billService = null;
+            var payrollCalculations = new Mock<Payroll>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new NonLaborContractController(mapService.Object, employeeService.Object, billService, payrollCalculations.Object));
+
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenPayrollIsNull()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            var billService = new Mock<IRemunerationBillService>();
+            Payroll payrollCalculations = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new NonLaborContractController(mapService.Object, employeeService.Object, billService.Object, payrollCalculations));
+        }
+    }
+}
---
+++ SalaryCalculator/SalaryCalculatorWeb.Tests/Controllers/NonLaborContractControllerTests/CreateLaborContract_Should.cs
@@ -0,0 +1,133 @@
+﻿using Moq;
+using NUnit.Framework;
+using SalaryCalculator.Configuration.Mappings;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Services.Contracts;
+using SalaryCalculator.Tests.Mocks;
+using SalaryCalculator.Utilities.Calculations;
+using SalaryCalculatorWeb.Controllers;
+using SalaryCalculatorWeb.Models.ContractViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SalaryCalculatorWeb.Tests.Controllers.NonLaborContractControllerTests
+{
+    [TestFixture]
+    public class CreateLaborContract_Should
+    {
+        [Test]
+        public void ReturnPreviewRemunerationBillViewModel_WhenModelStateIsValid()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            var billService = new Mock<IRemunerationBillService>();
+            var payrollCalculations = new Mock<Payroll>();
+
+            var id = 5;
+            var nonLaborContractModel = new Mock<PreviewRemunerationBillViewModel>();
+            nonLaborContractModel.SetupProperty(x => x.GrossSalary, 1000);
+
+            Employee mockedEmployee = new FakeEmployee()
+            {
+                Id = id,
+                FirstName = "Alexander",
+                MiddleName = "Petrov",
+                LastName = "Vasilev",
+                PersonalId = "8010107070"
+            };
+            employeeService.Setup(x => x.GetById(id)).Returns(mockedEmployee).Verifiable();
+            
+            // Act
+            NonLaborContractController laborController = new NonLaborContractController(mapService.Object, employeeService.Object, billService.Object, payrollCalculations.Object);
+
+            var result = laborController.CreateNonLaborContract(id, nonLaborContractModel.Object) as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.AreEqual("Details", result.ViewName);
+            Assert.AreEqual(nonLaborContractModel.Object, result.Model);
+        }
+
+        [Test]
+        public void ReturnCreateLaborContractViewModel_WhenModelStateIsNotValid()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            var billService = new Mock<IRemunerationBillService>();
+            var payrollCalculations = new Mock<Payroll>();
+
+            var id = 5;
+            var nonLaborContractModel = new Mock<PreviewRemunerationBillViewModel>();
+            nonLaborContractModel.SetupProperty(x => x.GrossSalary, 1000);
+
+            Employee mockedEmployee = new FakeEmployee()
+            {
+                Id = id,
+                FirstName = "Alexander",
+                MiddleName = "Petrov",
+                LastName = "Vasilev",
+                PersonalId = "8010107070"
+            };
+            employeeService.Setup(x => x.GetById(id)).Returns(mockedEmployee).Verifiable();
+            
+            // Act
+            NonLaborContractController laborController = new NonLaborContractController(mapService.Object, employeeService.Object, billService.Object, payrollCalculations.Object);
+            laborController.ModelState.AddModelError("invalid", "Invalid");
+
+            var result = laborController.CreateNonLaborContract(id, nonLaborContractModel.Object) as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.AreEqual(nonLaborContractModel.Object, result.Model);
+        }
+
+        [Test]
+        public void ReturnCreateRemunerationBillViewModel_WhenActionIsCalled()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            var billService = new Mock<IRemunerationBillService>();
+            var payrollCalculations = new Mock<Payroll>();
+
+            var id = 5;
+
+            Employee mockedEmployee = new FakeEmployee()
+            {
+                Id = id,
+                FirstName = "Alexander",
+                MiddleName = "Petrov",
+                LastName = "Vasilev",
+                PersonalId = "8010107070"
+            };
+
+            RemunerationBill mockedBill = new FakeRemunerationBill()
+            {
+                Id = 1,
+                EmployeeId = id,
+                Employee = mockedEmployee
+            };
+
+            employeeService.Setup(x => x.GetById(id)).Returns(mockedEmployee).Verifiable();
+
+            var nonLaborContractModel = new CreateRemunerationBillViewModel();
+            mapService.Setup(x => x.Map<CreateRemunerationBillViewModel>(mockedBill)).Returns(nonLaborContractModel).Verifiable();
+            
+            // Act
+            NonLaborContractController laborController = new NonLaborContractController(mapService.Object, employeeService.Object, billService.Object, payrollCalculations.Object);
+            laborController.ModelState.AddModelError("invalid", "Invalid");
+
+            var result = laborController.CreateNonLaborContract(id, mockedBill) as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.AreEqual(nonLaborContractModel, result.Model);
+        }
+    }
+}
