d642be873bafd7e9a6eb043e69cecd0c24c9a5da
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-18 21:44:27 +0200

    add more unit tests

---
+++ SalaryCalculator/SalaryCalculatorWeb.Tests/Controllers/FreelanceContractControllerTests/Constructor_Should.cs
@@ -0,0 +1,85 @@
+﻿using Moq;
+using NUnit.Framework;
+using SalaryCalculator.Configuration.Mappings;
+using SalaryCalculator.Data.Services.Contracts;
+using SalaryCalculator.Utilities.Calculations;
+using SalaryCalculatorWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorWeb.Tests.Controllers.FreelanceContractControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void CreateInstance_WhenAllServicesArePassedCorrectly()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            var selfEmploymentService = new Mock<ISelfEmploymentService>();
+            var payrollCalculations = new Mock<Payroll>();
+            
+            // Act & Assert
+            Assert.IsInstanceOf<FreelanceContractController>(new FreelanceContractController(mapService.Object, employeeService.Object, selfEmploymentService.Object, payrollCalculations.Object));
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenMapServiceIsNull()
+        {
+            // Arrange
+            IMapService mapService = null;
+            var employeeService = new Mock<IEmployeeService>();
+            var selfEmploymentService = new Mock<ISelfEmploymentService>();
+            var payrollCalculations = new Mock<Payroll>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new FreelanceContractController(mapService, employeeService.Object, selfEmploymentService.Object, payrollCalculations.Object));
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenEmployeeServiceIsNull()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            IEmployeeService employeeService = null;
+            var selfEmploymentService = new Mock<ISelfEmploymentService>();
+            var payrollCalculations = new Mock<Payroll>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new FreelanceContractController(mapService.Object, employeeService, selfEmploymentService.Object, payrollCalculations.Object));
+
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenselfEmploymentServiceIsNull()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            ISelfEmploymentService selfEmploymentService = null;
+            var payrollCalculations = new Mock<Payroll>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new FreelanceContractController(mapService.Object, employeeService.Object, selfEmploymentService, payrollCalculations.Object));
+
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenPayrollIsNull()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            var selfEmploymentService = new Mock<ISelfEmploymentService>();
+            Payroll payrollCalculations = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new FreelanceContractController(mapService.Object, employeeService.Object, selfEmploymentService.Object, payrollCalculations));
+        }
+    }
+}
---
+++ SalaryCalculator/SalaryCalculatorWeb.Tests/Controllers/FreelanceContractControllerTests/CreateFreelanceContract_Should.cs
@@ -0,0 +1,133 @@
+﻿using Moq;
+using NUnit.Framework;
+using SalaryCalculator.Configuration.Mappings;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Services.Contracts;
+using SalaryCalculator.Tests.Mocks;
+using SalaryCalculator.Utilities.Calculations;
+using SalaryCalculatorWeb.Controllers;
+using SalaryCalculatorWeb.Models.ContractViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SalaryCalculatorWeb.Tests.Controllers.FreelanceContractControllerTests
+{
+    [TestFixture]
+    public class CreateFreelanceContract_Should
+    {
+        [Test]
+        public void ReturnPreviewSelfEmploymentViewModel_WhenModelStateIsValid()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            var selfEmploymentService = new Mock<ISelfEmploymentService>();
+            var payrollCalculations = new Mock<Payroll>();
+
+            var id = 5;
+            var freelanceContractModel = new Mock<PreviewSelfEmploymentViewModel>();
+            freelanceContractModel.SetupProperty(x => x.GrossSalary, 1000);
+
+            Employee mockedEmployee = new FakeEmployee()
+            {
+                Id = id,
+                FirstName = "Alexander",
+                MiddleName = "Petrov",
+                LastName = "Vasilev",
+                PersonalId = "8010107070"
+            };
+            employeeService.Setup(x => x.GetById(id)).Returns(mockedEmployee).Verifiable();
+            
+            // Act
+            FreelanceContractController laborController = new FreelanceContractController(mapService.Object, employeeService.Object, selfEmploymentService.Object, payrollCalculations.Object);
+
+            var result = laborController.CreateFreelanceContract(id, freelanceContractModel.Object) as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.AreEqual("Details", result.ViewName);
+            Assert.AreEqual(freelanceContractModel.Object, result.Model);
+        }
+
+        [Test]
+        public void ReturnCreateLaborContractViewModel_WhenModelStateIsNotValid()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            var selfEmploymentService = new Mock<ISelfEmploymentService>();
+            var payrollCalculations = new Mock<Payroll>();
+
+            var id = 5;
+            var freelanceContractModel = new Mock<PreviewSelfEmploymentViewModel>();
+            freelanceContractModel.SetupProperty(x => x.GrossSalary, 1000);
+
+            Employee mockedEmployee = new FakeEmployee()
+            {
+                Id = id,
+                FirstName = "Alexander",
+                MiddleName = "Petrov",
+                LastName = "Vasilev",
+                PersonalId = "8010107070"
+            };
+            employeeService.Setup(x => x.GetById(id)).Returns(mockedEmployee).Verifiable();
+            
+            // Act
+            FreelanceContractController laborController = new FreelanceContractController(mapService.Object, employeeService.Object, selfEmploymentService.Object, payrollCalculations.Object);
+            laborController.ModelState.AddModelError("invalid", "Invalid");
+
+            var result = laborController.CreateFreelanceContract(id, freelanceContractModel.Object) as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.AreEqual(freelanceContractModel.Object, result.Model);
+        }
+
+        [Test]
+        public void ReturnCreateRemunerationBillViewModel_WhenActionIsCalled()
+        {
+            // Arrange
+            var mapService = new Mock<IMapService>();
+            var employeeService = new Mock<IEmployeeService>();
+            var selfEmploymentService = new Mock<ISelfEmploymentService>();
+            var payrollCalculations = new Mock<Payroll>();
+
+            var id = 5;
+
+            Employee mockedEmployee = new FakeEmployee()
+            {
+                Id = id,
+                FirstName = "Alexander",
+                MiddleName = "Petrov",
+                LastName = "Vasilev",
+                PersonalId = "8010107070"
+            };
+
+            SelfEmployment mockedSelfEmpl = new FakeSelfEmployment()
+            {
+                Id = 1,
+                EmployeeId = id,
+                Employee = mockedEmployee
+            };
+
+            employeeService.Setup(x => x.GetById(id)).Returns(mockedEmployee).Verifiable();
+
+            var freelanceContractModel = new CreateSelfEmploymentViewModel();
+            mapService.Setup(x => x.Map<CreateSelfEmploymentViewModel>(mockedSelfEmpl)).Returns(freelanceContractModel).Verifiable();
+            
+            // Act
+            FreelanceContractController laborController = new FreelanceContractController(mapService.Object, employeeService.Object, selfEmploymentService.Object, payrollCalculations.Object);
+            laborController.ModelState.AddModelError("invalid", "Invalid");
+
+            var result = laborController.CreateFreelanceContract(id, mockedSelfEmpl) as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.AreEqual(freelanceContractModel, result.Model);
+        }
+    }
+}
