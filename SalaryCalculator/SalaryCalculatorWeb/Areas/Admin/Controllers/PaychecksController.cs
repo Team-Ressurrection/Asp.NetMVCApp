26f0f35952f05442e52ea468688b4c7066397b9d
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-26 19:02:26 +0300

    add PaycheckController in admin area

---
+++ SalaryCalculator/SalaryCalculatorWeb/Areas/Admin/Controllers/PaychecksController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+using Bytes2you.Validation;
+
+using SalaryCalculator.Configuration.Mappings;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Services.Contracts;
+using SalaryCalculator.Utilities.Constants;
+using SalaryCalculator.Utilities.Pagination;
+using SalaryCalculatorWeb.Areas.Admin.Models;
+using SalaryCalculator.Utilities.Factories;
+using SalaryCalculatorWeb.Areas.Admin.Controllers.Base;
+
+namespace SalaryCalculatorWeb.Areas.Admin.Controllers
+{
+    [Authorize(Roles = ValidationConstants.AdminRole)]
+    public class PaychecksController : BaseController
+    {
+        private readonly IEmployeePaycheckService employeePaycheckService;
+
+        public PaychecksController(IMapService mapService, IPagerFactory pagerFactory, IEmployeePaycheckService employeePaycheckService)
+            : base(mapService, pagerFactory)
+        {
+            Guard.WhenArgument<IEmployeePaycheckService>(employeePaycheckService, "employeePaycheckService").IsNull().Throw();
+
+            this.employeePaycheckService = employeePaycheckService;
+        }
+
+        // GET: Admin/Employees
+        [HttpGet]
+        public ActionResult Index(int? page, PageViewModel<PaycheckViewModel> pageViewModel)
+        {
+
+            var paychecks = this.employeePaycheckService.GetAll().OrderBy(x=> x.Id).AsEnumerable();
+            var pager = this.pagerFactory.CreatePager(paychecks.Count(), page);
+
+            pageViewModel.Items = this.mapService.Map<IEnumerable<PaycheckViewModel>>(paychecks)
+                                                 .Skip((pager.CurrentPage - 1) * pager.PageSize)
+                                                 .Take(pager.PageSize);
+            pageViewModel.Pager = pager;
+
+            return View("Index",pageViewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id, PaycheckViewModel paycheckModel)
+        {
+            var paycheck = this.employeePaycheckService.GetById(id);
+            if (paycheck == null)
+            {
+                return HttpNotFound();
+            }
+            paycheckModel = this.mapService.Map<PaycheckViewModel>(paycheck);
+
+            return View("Edit", paycheckModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(PaycheckViewModel paycheckModel)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var paycheck = this.mapService.Map<EmployeePaycheck>(paycheckModel);
+
+                this.employeePaycheckService.UpdateById(paycheck.Id, paycheck);
+                return RedirectToAction("Index", "Paychecks");
+            }
+            return View(paycheckModel);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id, EmployeePaycheck paycheck)
+        {
+            paycheck = this.employeePaycheckService.GetById(id);
+            if (paycheck == null)
+            {
+                return HttpNotFound();
+            }
+
+            var paycheckViewModel = this.mapService.Map<PaycheckViewModel>(paycheck);
+            return View(paycheckViewModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id, PaycheckViewModel paycheckViewModel)
+        {
+            this.employeePaycheckService.DeleteById(id);
+
+            return RedirectToAction("Index", "Paychecks");
+        }
+    }
+}
---
+++ SalaryCalculator/SalaryCalculatorWeb/Areas/Admin/Models/PaycheckViewModel.cs
@@ -0,0 +1,32 @@
+﻿using SalaryCalculator.Configuration.Mappings;
+using SalaryCalculator.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalaryCalculatorWeb.Areas.Admin.Models
+{
+    public class PaycheckViewModel : IMapFrom<EmployeePaycheck>
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public virtual decimal GrossSalary { get; set; }
+
+        public virtual decimal GrossFixedBonus { get; set; }
+
+        public virtual decimal GrossNonFixedBonus { get; set; }
+
+        public virtual decimal SocialSecurityIncome { get; set; }
+
+        public virtual decimal PersonalInsurance { get; set; }
+
+        public virtual decimal IncomeTax { get; set; }
+
+        public virtual decimal NetWage { get; set; }
+
+        public virtual int EmployeeId { get; set; }
+    }
+}
