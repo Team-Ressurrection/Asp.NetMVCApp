04a04ee5e109dcbeb07aabf7af28598085842730
Alex Nestorov <nestorov.alexander@gmail.com>
2017-03-26 21:41:02 +0300

    namespace fixes

--- SalaryCalculator/SalaryCalculatorWeb/Areas/Admin/Models/PaycheckViewModel.cs
+++ SalaryCalculator/SalaryCalculatorWeb/Areas/Admin/Models/PaycheckViewModel.cs
@@ -1,9 +1,7 @@
-﻿using SalaryCalculator.Configuration.Mappings;
+﻿using System;
+
+using SalaryCalculator.Configuration.Mappings;
 using SalaryCalculator.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace SalaryCalculatorWeb.Areas.Admin.Models
 {
