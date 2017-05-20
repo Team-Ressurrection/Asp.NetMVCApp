d794e817db7cb211a49304482b79f802b7bd2657
Alex Nestorov <nestorov.alexander@gmail.com>
2017-05-20 14:57:55 +0300

    edit and delete now works

--- SalaryCalculator/SalaryCalculatorWeb/Areas/Admin/Controllers/BillsController.cs
+++ SalaryCalculator/SalaryCalculatorWeb/Areas/Admin/Controllers/BillsController.cs
@@ -1,17 +1,21 @@
-﻿using SalaryCalculatorWeb.Areas.Admin.Controllers.Base;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+
+using Bytes2you.Validation;
+
 using SalaryCalculator.Configuration.Mappings;
 using SalaryCalculator.Utilities.Factories;
 using SalaryCalculator.Data.Services.Contracts;
-using Bytes2you.Validation;
+using SalaryCalculator.Utilities.Constants;
+using SalaryCalculator.Data.Models;
+
+using SalaryCalculatorWeb.Areas.Admin.Controllers.Base;
 using SalaryCalculatorWeb.Areas.Admin.Models;
 
 namespace SalaryCalculatorWeb.Areas.Admin.Controllers
 {
+    [Authorize(Roles = ValidationConstants.AdminRole)]
     public class BillsController : BaseController
     {
         private readonly IRemunerationBillService billService;
@@ -36,6 +40,55 @@ namespace SalaryCalculatorWeb.Areas.Admin.Controllers
             pageViewModel.Pager = pager;
 
             return View("Index", pageViewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id, BillViewModel billViewModel)
+        {
+            var remunerationBill = this.billService.GetById(id);
+            if (remunerationBill == null)
+            {
+                return HttpNotFound();
+            }
+            billViewModel = this.mapService.Map<BillViewModel>(remunerationBill);
+
+            return View("Edit", billViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BillViewModel billViewModel)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var remunerationBill = this.mapService.Map<RemunerationBill>(billViewModel);
+
+                this.billService.UpdateById(remunerationBill.Id, remunerationBill);
+                return RedirectToAction("Index", "Bills");
+            }
+            return View(billViewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id, RemunerationBill bill)
+        {
+            bill = this.billService.GetById(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+
+            var billViewModel = this.mapService.Map<BillViewModel>(bill);
+            return View(billViewModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id, BillViewModel billViewModel)
+        {
+            this.billService.DeleteById(id);
+
+            return RedirectToAction("Index", "Bills");
         }
     }
 }
