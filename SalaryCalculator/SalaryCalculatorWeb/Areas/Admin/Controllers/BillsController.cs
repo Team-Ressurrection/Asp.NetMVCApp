de88baded29c7b431e6c9106c67982d1f193e53f
Alex Nestorov <nestorov.alexander@gmail.com>
2017-05-20 14:29:16 +0300

    add remuneration bills in admin panel

---
+++ SalaryCalculator/SalaryCalculatorWeb/Areas/Admin/Controllers/BillsController.cs
@@ -0,0 +1,41 @@
+﻿using SalaryCalculatorWeb.Areas.Admin.Controllers.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SalaryCalculator.Configuration.Mappings;
+using SalaryCalculator.Utilities.Factories;
+using SalaryCalculator.Data.Services.Contracts;
+using Bytes2you.Validation;
+using SalaryCalculatorWeb.Areas.Admin.Models;
+
+namespace SalaryCalculatorWeb.Areas.Admin.Controllers
+{
+    public class BillsController : BaseController
+    {
+        private readonly IRemunerationBillService billService;
+
+        public BillsController(IMapService mapService, IPagerFactory pagerFactory, IRemunerationBillService billService) 
+            : base(mapService, pagerFactory)
+        {
+            Guard.WhenArgument<IRemunerationBillService>(billService, "billService").IsNull().Throw();
+
+            this.billService = billService;
+        }
+
+        // GET: Admin/Bills
+        public ActionResult Index(int? page, PageViewModel<BillViewModel> pageViewModel)
+        {
+            var remunerationBills = this.billService.GetAll().OrderBy(x => x.Id).AsEnumerable();
+            var pager = this.pagerFactory.CreatePager(remunerationBills.Count(), page);
+
+            pageViewModel.Items = this.mapService.Map<IEnumerable<BillViewModel>>(remunerationBills)
+                                                 .Skip((pager.CurrentPage - 1) * pager.PageSize)
+                                                 .Take(pager.PageSize);
+            pageViewModel.Pager = pager;
+
+            return View("Index", pageViewModel);
+        }
+    }
+}
---
+++ SalaryCalculator/SalaryCalculatorWeb/Areas/Admin/Models/BillViewModel.cs
@@ -0,0 +1,28 @@
+﻿using SalaryCalculator.Configuration.Mappings;
+using SalaryCalculator.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalaryCalculatorWeb.Areas.Admin.Models
+{
+    public class BillViewModel : IMapFrom<RemunerationBill>
+    {
+        public virtual int Id { get; set; }
+
+        public virtual DateTime CreatedDate { get; set; }
+
+        public virtual decimal GrossSalary { get; set; }
+
+        public virtual decimal SocialSecurityIncome { get; set; }
+
+        public virtual decimal PersonalInsurance { get; set; }
+
+        public virtual decimal IncomeTax { get; set; }
+
+        public virtual decimal NetWage { get; set; }
+
+        public virtual int EmployeeId { get; set; }
+    }
+}
